00bab8c189ac96f14371721842a1f1af2fdde6b5
craigsturzaker <craig.sturzaker@arup.com>
2022-03-09 10:58:00 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arc-ddd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ddd-arc';
+}
